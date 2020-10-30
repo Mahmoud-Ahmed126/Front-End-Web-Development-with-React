ca143c80843677a4293acd2c487a6f36e5e548eb
Mahmoud Ahmed <m.ahmedhussien44@gmail.com>
2020-10-30 16:27:04 +0200

    Assignment 1 And Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent'; 
+import React, { Component } from 'react';
+import Main from './components/MainComponent'; 
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Restorant Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-      {/* self colsing />  */}
-    </div>
-  );
+class App extends Component
+{
+ 
+  render () {
+    return (
+      <div>
+      
+        <Main />
+        {/* self colsing />  */}
+      </div>
+    );
+  }
+  
+  
 }
-
 export default App;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,82 +1,83 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 class Menu extends Component
 {
-    constructor(props)
-    {
-        /*
+     /*
         The super keyword refers to the parent class.
         It is used to call the constructor of the parent class and to access the parent's properties and methods.
         */
        /*
+       state is a javascript object 
        state => Each component can store its own local information in its "state"
          - private and fully controlled by the component 
          - can be passed as props to children
+         - only class component can have local state 
+
        */
+      /*
+      props => JSX attributes are passed into a component as a single object
+      */
+    constructor(props)
+    {
         super(props);
-        this.state = {
-            dishes:[
-                {
-                    id: 0,
-                    name:'Uthappizza',
-                    image: 'assets/images/uthappizza.png',
-                    category: 'mains',
-                    label:'Hot',
-                    price:'4.99',
-                    description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
-                 {
-                    id: 1,
-                    name:'Zucchipakoda',
-                    image: 'assets/images/zucchipakoda.png',
-                    category: 'appetizer',
-                    label:'',
-                    price:'1.99',
-                    description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
-                 {
-                    id: 2,
-                    name:'Vadonut',
-                    image: 'assets/images/vadonut.png',
-                    category: 'appetizer',
-                    label:'New',
-                    price:'1.99',
-                    description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
-                 {
-                    id: 3,
-                    name:'ElaiCheese Cake',
-                    image: 'assets/images/elaicheesecake.png',
-                    category: 'dessert',
-                    label:'',
-                    price:'2.99',
-                    description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        }
-            ]
+       
+        console.log('Menu Component Constructor is invoked');
+    }
+    componentDidMount()
+    {
+        console.log('Menu Component ComponentDidMount is invoked');
+    }
+    // when need to change state must use setState js object
+    
+    renderDish(dish)
+    {
+        if(dish != null)
+        {
+            return (
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                    <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }else{
+            return (
+                <div></div>
+            );
         }
     }
     render()
     {
         // iterate over all the items in my dishes array using map 
-        const menu = this.state.dishes.map( (dish) => {
+        const menu = this.props.dishes.map( (dish) => {
             return (
 
-                <div key={dish.id} className="col-12 mt-5">
-                    <Media tag="li">
-                        <Media left middle>
-                            <Media object src={dish.image} alt={dish.name}/>
-                        </Media>
-                        <Media body className="ml-5">
-            <Media heading>{dish.name}</Media>
-            <p>{dish.description}</p>
-                        </Media>
-                    </Media>
+                <div key={dish.id} className="col-12 col-md-5 mt-1">
+                    <Card onClick={ () => this.props.onClick(dish.id) }>
+                        
+                            <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                        
+                        <CardImgOverlay>
+                          <CardTitle>{dish.name}</CardTitle>
+            
+                        </CardImgOverlay>
+                    </Card>
                 </div>
             ); 
             
         });
+        console.log('Menu Component render is invoked');
+
         return (
             <div className="container">
                 <div className="row">
-                    <Media list>
                         {menu}
-                    </Media>
+                </div>
+                <div className="row">
+                    {/* {this.renderDish(this.state.selectedDish)} */}
+                    
                 </div>
             </div>
 
